6a9833f6227134220d60fe39110214a284ada078
Brixton <24017600@saultcollege.ca>
2026-07-08 13:18:47 -0400

    Dockerfile Ready

--- Project Folder/config/robot_config.py
+++ Project Folder/config/robot_config.py
@@ -1,1 +1,21 @@
-ROBOT_IP='192.168.80.3'
+import os
+
+DEFAULT_HOSTNAME = os.getenv("SPOT_HOSTNAME", "192.186.80.3")
+DEFAULT_USERNAME = os.getenv("SPOT_USERNAME", "user")
+DEFAULT_PASSWORD = os.getenv("SPOT_PASSWORD", "mz9jmfhp68o7")
+
+def get_robot_config():
+    return {
+        "hostname": DEFAULT_HOSTNAME,
+        "username": DEFAULT_USERNAME,
+        "password": DEFAULT_PASSWORD
+    }
+
+def get_hostname():
+    return DEFAULT_HOSTNAME
+
+def get_username():
+    return DEFAULT_USERNAME
+
+def get_password():
+    return DEFAULT_PASSWORD
